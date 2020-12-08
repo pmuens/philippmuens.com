eb33ee08ab829148fcc06cd6a3a4dde3e48a7916
Philipp Muens <raute1337@gmx.de>
2020-12-08 19:29:22 +0100

    Update to use React Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,32 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { DefaultSeo } from 'next-seo'
 import '../styles/index.css'
 import '../styles/prism-theme.css'
+import { initGA, logPageView } from '../lib/analytics'
 import { domain, blogName, blogDescription } from '../core.config'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    initGA()
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes('?')) {
+      logPageView()
+    }
+  }, [])
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on('routeChangeComplete', logPageView)
+    return () => {
+      router.events.off('routeChangeComplete', logPageView)
+    }
+  }, [router.events])
+
   return (
     <>
       <DefaultSeo
--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,10 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { trackingId } from '../core.config'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`} />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${trackingId}');`,
-            }}
-          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
