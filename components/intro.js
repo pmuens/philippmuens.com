6758d3160edc04f4ab41a93bda686802fb8b5fbf
Philipp Muens <philipp@muens.io>
2024-02-04 18:22:33 +0100

    Remove link to about page

--- components/intro.js
+++ components/intro.js
@@ -1,10 +1,7 @@
-import Link from 'next/link'
 import { blogName, blogDescription } from '../core.config'
-import { getPostPath } from '../lib/utils'
 
 export default function Intro() {
   let description = blogDescription.split(' ')
-  description[0] = description[0].toLowerCase()
   description = description.join(' ')
 
   return (
@@ -12,12 +9,7 @@ export default function Intro() {
       <h1 className="text-6xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
         {blogName}
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        <Link as={getPostPath('about-me')} href={getPostPath('about-me')}>
-          <a className="underline">My</a>
-        </Link>{' '}
-        {description}
-      </h4>
+      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">{description}</h4>
     </section>
   )
 }
